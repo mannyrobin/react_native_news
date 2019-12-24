a0477a41487d6d9b25febb9473044c0768a526da
mannyrobin <69haiqian@gmail.com>
2019-12-25 05:33:43 +1000

    Added home screen menu icons.

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -4,7 +4,10 @@ import {
   TextInput,
   StyleSheet,
   View,
+  Image,
 } from 'react-native';
+
+import Swiper from 'react-native-page-swiper'
 
 import { MonoText } from '../components/StyledText';
 import Colors from '../constants/Colors';
@@ -13,8 +16,96 @@ export default function NewsScreen() {
   return (
     <View style={styles.container}>
       <View>
-        <TextInput placeholder="Enter search key..." style={styles.searchBox}/>
-      </View>      
+        <TextInput placeholder="Enter search key..." style={styles.searchbox}/>
+        <Swiper style={menuicons.wrapper} pager={true} activeDotColor={"white"}>
+          <View style={menuicons.slide1}>
+            <View style={{flex: 1, flexDirection: "row", width: '100%'}}>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+            </View>
+            <View style={{flex: 1, flexDirection: "row", width: '100%'}}>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+            </View>
+          </View>
+          <View style={menuicons.slide2}>
+            <View style={{flex: 1, flexDirection: "row", width: '100%'}}>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+            </View>
+            <View style={{flex: 1, flexDirection: "row", width: '100%'}}>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+              <View style={menuicons.item}>
+                <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+                <MonoText style={menuicons.text}>Такси</MonoText>
+              </View>
+            </View>            
+          </View>
+          <View style={menuicons.slide3}>
+            <View style={menuicons.item}>
+              <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+              <MonoText style={menuicons.text}>Такси</MonoText>
+            </View>
+            <View style={menuicons.item}>
+              <Image source={require("../assets/images/news_header_icon/news.png")} style={menuicons.image}/>
+              <MonoText style={menuicons.text}>Такси</MonoText>
+            </View>
+          </View>
+        </Swiper>
+      </View>
     </View>
   );
 }
@@ -40,8 +131,61 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.mainBackground,
   },
-  searchBox: {
-    background: 'white',
-    borderRadius: 
+  searchbox: {
+    backgroundColor: 'white',
+
+    marginRight: '10%',
+    marginLeft: '10%',
+    marginTop: 15,
+
+    paddingTop:10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+
+    fontSize: 25,
+    borderRadius: 5,
   },
 });
+
+const menuicons = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'none',
+    height: 400,
+    padding: 30,
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'none',
+    height: 400,
+  },
+  slide3: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'none',
+    height: 400,
+  },
+  item: {
+    width: '25%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  }
+})
